057fad77e58fa16d5372d61c90f9e8df091cc00d
normanjd <48414656+normanjd@users.noreply.github.com>
2019-03-12 17:07:33 -0400

    Update onelanebridge.c

--- onelanebridge.c
+++ onelanebridge.c
@@ -1,78 +1,89 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 
 
+//One Lane Bridge Monitor Solution
 
+int thread_ids[50];
+pthread_mutex_t count_mutex;
+pthread_cond_t count_threshold_cv;
 
-//Monitor Bridge {
-    int nWaiting=0, sWaiting=0, sOnBridge=0, nOnBridge=0;
-    pthread_cond_t north_turn, south_turn;
-    pthread_mutex_t count_mutex;
-    void enterNorth() {
-        if (sWaiting>0 || sOnBridge>0)
-        {
-            nWaiting++;
-            pthread_cond_wait(&north_turn,&count_mutex);
-            while (sOnBridge>0)
-                pthread_cond_wait(&north_turn,&count_mutex);
-            nWaiting--;
-        }
-        nOnBridge++;
-        if (sWaiting==0)
-        {
-            pthread_mutex_lock(&count_mutex);
-            pthread_cond_signal(&north_turn);
-        }
+//monitor Bridge {
+    //Condition safe;
+    
+    int currentDirec;
+    int currentNumber;
+
+    //don't lock here or else you'll get deadlock
+    bool isSafe(int carID) {
+        if ( currentNumber == 0 )
+            return true;    // always safe when bridge is empty
+        else if ((currentNumber < 3) && (currentDirec == carID))
+            return true;    // room for us to follow others in direc
+        else
+            return false;   // bridge is full or has oncoming traffic.
     }
-    void exitNorth() {
-        nOnBridge--;
-        pthread_mutex_unlock(&count_mutex);
-        if (nOnBridge ==0)
-            pthread_cond_signal(&south_turn);
-    }
-    void enterSouth() {
-        if (nWaiting>0 || nOnBridge>0)
-        {
-            sWaiting++;
-            pthread_cond_wait(&south_turn,&count_mutex);
-            while (nOnBridge>0)
-                pthread_cond_wait(&south_turn,&count_mutex);
-            sWaiting--;
-        }
-        sOnBridge++;
+    void ArriveBridge(int carID) {
+        printf("A car in direction %d has arrived at the bridge\n",carID);
         pthread_mutex_lock(&count_mutex);
-        if (nWaiting==0)
-            pthread_cond_signal(&south_turn);
+        while (!isSafe(carID) )
+            pthread_cond_wait(&count_threshold_cv, &count_mutex);
+        currentNumber++;
+        currentDirec = carID;
+        printf("current number %d current direction %d\n", currentNumber, currentDirec);
         pthread_mutex_unlock(&count_mutex);
     }
-    void exitSouth() {
-        sOnBridge--;
-        if(sOnBridge == 0)
-            pthread_cond_signal(&north_turn);
+    void CrossBridge (int carID) {
+        pthread_mutex_lock(&count_mutex);
+        printf("A car is crossing the bridge with direction %d\n", carID);
+        pthread_mutex_unlock(&count_mutex);
     }
-//}
+    void ExitBridge(int carID) {
+        pthread_mutex_lock(&count_mutex);
+        currentNumber--;
+        printf("A car is exiting the bridge from direction %d\n\n", carID);
+        //safe.Signal();
+        pthread_mutex_unlock(&count_mutex);
+    }
 
-int main() {
+    
+//}
+void OneVehicle(int carID) {
+    ArriveBridge(carID);
+    CrossBridge(carID);
+    ExitBridge(carID);
+}
+
+int main()
+{
+    pthread_t threads[50];
+    pthread_attr_t attr;
+
     pthread_mutex_init(&count_mutex, NULL);
     pthread_cond_init (&count_threshold_cv, NULL);
     
-    /*
-     For portability, explicitly create threads in a joinable state
-     */
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_create(&threads[0], &attr, inc_count, (void *)&thread_ids[0]);
-    pthread_create(&threads[1], &attr, inc_count, (void *)&thread_ids[1]);
-    pthread_create(&threads[2], &attr, watch_count, (void *)&thread_ids[2]);
-     
-     /* Wait for all threads to complete */
-    for (i = 0; i < NUM_THREADS; i++) {
-     pthread_join(threads[i], NULL);
-     }
-     //printf ("Main(): Waited on %d  threads. Done.\n", NUM_THREADS);
-    
-     
-     /* Clean up and exit */
-     pthread_attr_destroy(&attr);
-     pthread_mutex_destroy(&count_mutex);
-     pthread_cond_destroy(&count_threshold_cv);
-     pthread_exit (NULL);
+    for(int i=0; i<50; i++)
+        pthread_create(&threads[i], &attr, <#void * _Nullable (* _Nonnull)(void * _Nullable)#>, (void *)&thread_ids[i]);
+
+    srand(time(NULL));
+    for(int i=0; i<50; i++)
+        thread_ids[i] = rand() % 2;
+    for(int i=0; i<50; i++){
+        OneVehicle(thread_ids[i]);
+        
+        for (i = 0; i < 50; i++) {
+            pthread_join(threads[i], NULL);
+        }
+        pthread_attr_destroy(&attr);
+        pthread_mutex_destroy(&count_mutex);
+        pthread_cond_destroy(&count_threshold_cv);
+        pthread_exit (NULL);
+    }
 }
